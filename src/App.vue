e0ca426d66d03fcb01d27c89ef6dfb9c4dab8b9b
ddoongg <2945705414@qq.com>
2020-12-07 15:31:59 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from "./components/FooterGuide/FooterGuide.vue";
+  import {reqFoodTyoes} from "./api";
+
+  export default {
+
+    async mounted() {
+      // const await reqFoodTypes()
+    },
+
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
